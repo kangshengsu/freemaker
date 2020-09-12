39a148f17467ad2b6566bdc25abdc6bb04c72bc8
hubo <hubo02090209@sina.com>
2020-09-12 11:34:27 +0800

    完善web模块

---
+++ api-web/src/main/java/com/fm/api/web/ApiWebStarter.java
@@ -0,0 +1,18 @@
+package com.fm.api.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Starter
+ * @author clufeng
+ * @version 1.0.0
+ **/
+@SpringBootApplication
+public class ApiWebStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiWebStarter.class, args);
+    }
+
+}
