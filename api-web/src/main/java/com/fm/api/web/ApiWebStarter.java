b510b24a524daeea346c36d67b94c37b951b2c25
zhangleqi <463456910@qq.com>
2020-09-13 10:38:07 +0800

    环境搭建: 集成caffeine

--- api-web/src/main/java/com/fm/api/web/ApiWebStarter.java
+++ api-web/src/main/java/com/fm/api/web/ApiWebStarter.java
@@ -3,6 +3,7 @@ package com.fm.api.web;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  **/
 @SpringBootApplication(scanBasePackages = "com.fm")
 @MapperScan("com.fm.**.dao")
+@EnableCaching
 public class ApiWebStarter {
 
     public static void main(String[] args) {
