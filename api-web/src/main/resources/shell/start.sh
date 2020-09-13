7864df30fe8fb588a55782bc439983a488dcf583
hubo <hubo02090209@sina.com>
2020-09-13 20:12:15 +0800

    调试脚本2

--- api-web/src/main/resources/shell/start.sh
+++ api-web/src/main/resources/shell/start.sh
@@ -2,25 +2,15 @@
 
 LOG_DIR=`pwd`/logs
 
-echo "日志目录：${LOG_DIR}"
-
-mkdir -p ${LOG_DIR}
-
 JAR_NAME=`ls | grep api-web-*.jar`
 
-echo "jar名称：${JAR_NAME}"
-
 echo "JAVA_HOME：${JAVA_HOME}"
-
-source /etc/profile
-
-PATH=${JAVA_HOME}/bin:$PATH
 
 export PATH
 echo `java -version`
 
-nohup java -Xms512m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError \
+${JAVA_HOME}/bin/java -Xms512m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError \
  -XX:HeapDumpPath=${LOG_DIR} -XX:ErrorFile=${LOG_DIR}/java_error_%p.log \
- -jar `pwd`/${JAR_NAME} > ${LOG_DIR}/api-web.log 2>&1 &
+ -jar `pwd`/${JAR_NAME}
 
 echo "api-web 启动成功..."
