96877be64cddb259e9f21c35ecc4076746032bbb
hubo <hubo02090209@sina.com>
2020-09-13 17:15:47 +0800

    添加启动和停止脚本

---
+++ api-web/src/main/resources/shell/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+LOG_DIR =`pwd`/logs
+
+echo "日志目录：${LOG_DIR}"
+
+mkdir -p ${LOG_DIR}
+
+JAR_NAME = `ls | grep api-web-*.jar`
+
+echo "jar名称：${JAR_NAME}"
+
+nohup java -Xms512m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError \
+ -XX:HeapDumpPath=${LOG_DIR} -XX:ErrorFile=${LOG_DIR}/java_error_%p.log \
+ -jar `pwd`/${JAR_NAME} > ${LOG_DIR}/api-web.log 2>&1 &
+
+echo "api-web 启动成功..."
