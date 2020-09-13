096a1e4bc555f3713746b2c070163062ba8bcb5b
hubo <hubo02090209@sina.com>
2020-09-13 19:38:11 +0800

    脚本中添加javahome

--- api-web/src/main/resources/shell/start.sh
+++ api-web/src/main/resources/shell/start.sh
@@ -10,7 +10,9 @@ JAR_NAME=`ls | grep api-web-*.jar`
 
 echo "jar名称：${JAR_NAME}"
 
-nohup java -Xms512m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError \
+echo "JAVA_HOME：${JAVA_HOME}"
+
+nohup ${JAVA_HOME}/bin/java -Xms512m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError \
  -XX:HeapDumpPath=${LOG_DIR} -XX:ErrorFile=${LOG_DIR}/java_error_%p.log \
  -jar `pwd`/${JAR_NAME} > ${LOG_DIR}/api-web.log 2>&1 &
 
