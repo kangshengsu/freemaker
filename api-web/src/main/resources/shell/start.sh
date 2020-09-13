cbb475b593e01550a3dc8eeef6e46cd4b38468df
hubo <hubo02090209@sina.com>
2020-09-13 23:23:13 +0800

    改回nohup

--- api-web/src/main/resources/shell/start.sh
+++ api-web/src/main/resources/shell/start.sh
@@ -16,6 +16,6 @@ echo "JAVA_HOME：${JAVA_HOME}"
 
 source /etc/profile
 
-${JAVA_HOME}/bin/java -Xms512m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${LOG_DIR} -XX:ErrorFile=${LOG_DIR}/java_error_%p.log -jar `pwd`/${JAR_NAME}
+nohup ${JAVA_HOME}/bin/java -Xms512m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${LOG_DIR} -XX:ErrorFile=${LOG_DIR}/java_error_%p.log -jar `pwd`/${JAR_NAME} > ${LOG_DIR}/api-web.log 2>&1 &
 
 echo "api-web 启动成功..."
