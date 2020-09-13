2423b99a370dccbc99718bc86eb5ebc8b52340c3
hubo <hubo02090209@sina.com>
2020-09-13 19:46:36 +0800

    解决nohup不运行的问题

--- api-web/src/main/resources/shell/start.sh
+++ api-web/src/main/resources/shell/start.sh
@@ -12,6 +12,8 @@ echo "jar名称：${JAR_NAME}"
 
 echo "JAVA_HOME：${JAVA_HOME}"
 
+source /etc/profile
+
 nohup ${JAVA_HOME}/bin/java -Xms512m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError \
  -XX:HeapDumpPath=${LOG_DIR} -XX:ErrorFile=${LOG_DIR}/java_error_%p.log \
  -jar `pwd`/${JAR_NAME} > ${LOG_DIR}/api-web.log 2>&1 &
