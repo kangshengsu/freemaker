0f2c364a8f52660f59f4804ba8fd08c9f148782a
hubo <hubo02090209@sina.com>
2020-09-14 23:17:10 +0800

    完善关闭进程

--- api-web/src/main/resources/shell/stop.sh
+++ api-web/src/main/resources/shell/stop.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-ps -ef | grep api-web-*.jar | grep -v grep | awk '{print $2}' | xargs kill
+PID=`ps -ef | grep api-web-*.jar | grep -v grep | awk '{print $2}'`
+
+echo "正在关闭${PID}进程...."
+
+kill ${PID}
 
 sleep 10s
+
+echo "进程${PID}成功...."
