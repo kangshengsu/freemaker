0655209d695f17674dd185822950b7d0e2a52891
hubo <hubo02090209@sina.com>
2020-09-14 23:08:21 +0800

    查看任务

--- api-web/src/main/resources/shell/start.sh
+++ api-web/src/main/resources/shell/start.sh
@@ -18,4 +18,6 @@ source /etc/profile
 
 nohup ${JAVA_HOME}/bin/java -Xms512m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${LOG_DIR} -XX:ErrorFile=${LOG_DIR}/java_error_%p.log -jar `pwd`/${JAR_NAME} > ${LOG_DIR}/api-web.log 2>&1 &
 
+echo `jobs`
+
 echo "api-web 启动成功..."
