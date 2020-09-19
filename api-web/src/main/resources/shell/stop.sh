7a7b656b9b9b8455f60f0a536125c473abf573c9
hubo <hubo02090209@sina.com>
2020-09-19 11:13:35 +0800

    提交脚本

--- api-web/src/main/resources/shell/stop.sh
+++ api-web/src/main/resources/shell/stop.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-PID=`ps -ef | grep api-web-*.jar | grep -v grep | awk '{print $2}'`
-
-echo "正在关闭${PID}进程...."
-
-kill ${PID}
+`ps -ef | grep api-web-*.jar | grep -v grep | awk '{print $2}'` | xargs kill
 
 sleep 10s
 
-echo "关闭进程${PID}成功...."
+echo "关闭进程成功...."
