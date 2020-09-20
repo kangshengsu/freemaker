e8b3c9f7961ddae8650710bbc0fc18251cb14c75
hubo <hubo02090209@sina.com>
2020-09-20 11:40:40 +0800

    提交脚本5

--- api-web/src/main/resources/shell/stop.sh
+++ api-web/src/main/resources/shell/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ps -ef | grep `pwd` | grep -v grep | awk '{print $2}' | xargs kill
+ps -ef | grep 'api-web-1.0-SNAPSHOT.jar' | grep -v grep | awk '{print $2}' | xargs kill
 
 sleep 10s
 
