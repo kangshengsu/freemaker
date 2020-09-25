0f6e9989106b401b6a84fe3634ec75da3ee3fff2
liuduo <liuduo0827@163.com>
2020-09-26 00:23:58 +0800

    修改小程序作品接口返回结构

--- api-gw/src/main/java/com/fm/api/gw/controller/production/ProductionApi.java
+++ api-gw/src/main/java/com/fm/api/gw/controller/production/ProductionApi.java
@@ -6,25 +6,38 @@
 */
 package com.fm.api.gw.controller.production;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fm.api.gw.vo.freelancer.FreelancerInfoApiVO;
+import com.fm.api.gw.vo.production.AttachmentInfoApiVO;
 import com.fm.api.gw.vo.production.ProductionApiVO;
 import com.fm.business.base.enums.ProductionStatus;
+import com.fm.business.base.model.job.BdJobCate;
 import com.fm.business.base.model.production.ProductionInfo;
+import com.fm.business.base.model.production.ProductionSkillRelation;
 import com.fm.business.base.service.production.IProductionInfoService;
+import com.fm.framework.core.Context;
 import com.fm.framework.core.query.Page;
 import com.fm.framework.core.service.Service;
+import com.fm.framework.core.utils.JsonUtil;
 import com.fm.framework.web.controller.BaseController;
 import com.fm.framework.web.response.ApiResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
 *
@@ -34,9 +47,10 @@ import java.util.Set;
 * @time    2020年09月11日
 *
 */
-@Api(value = "作品接口")
+@Api(value = "作品接口",tags={"作品操作相关接口"})
 @RestController
 @RequestMapping("/v1/productionApi")
+@Validated
 public class ProductionApi extends BaseController<ProductionInfo,ProductionApiVO>{
 
     @Autowired
@@ -53,7 +67,10 @@ public class ProductionApi extends BaseController<ProductionInfo,ProductionApiVO
     @PostMapping("/release")
     @ApiOperation(value="发布作品")
     @ApiImplicitParam(name = "apiVO", value = "作品相关属性", dataType = "ProductionApiVO")
-    public ApiResponse<Boolean> release(@RequestBody ProductionApiVO apiVO){
+    public ApiResponse<Boolean> release(@RequestBody @Validated(value = {ProductionApiVO.Release.class}) ProductionApiVO apiVO){
+
+        //获取发布作者
+        apiVO.setFreelancerId(Context.getCurrUser());
 
         productionInfoService.save(convert(apiVO));
 
@@ -70,7 +87,7 @@ public class ProductionApi extends BaseController<ProductionInfo,ProductionApiVO
     @PostMapping("/modify")
     @ApiOperation(value="修改作品")
     @ApiImplicitParam(name = "apiVO", value = "作品相关属性", dataType = "ProductionApiVO")
-    public ApiResponse<Boolean> modify(@RequestBody ProductionApiVO apiVO){
+    public ApiResponse<Boolean> modify(@RequestBody @Validated(value = {ProductionApiVO.Modify.class}) ProductionApiVO apiVO){
 
         productionInfoService.update(convert(apiVO));
 
@@ -127,7 +144,7 @@ public class ProductionApi extends BaseController<ProductionInfo,ProductionApiVO
             @ApiImplicitParam(name="pageSize",value="每页数量",dataType="Integer")})
     public ApiResponse<Page<ProductionApiVO>> getByCateSkill(@RequestParam("currentPage") Integer currentPage,
                                                             @RequestParam("pageSize") Integer pageSize,
-                                                            @RequestParam("cateSkill") Long cateSkill){
+                                                            @RequestParam("cateSkill") @Validated() Long cateSkill){
 
         return success(convert(productionInfoService.findByCateSkill(currentPage,pageSize,cateSkill)));
 
@@ -135,15 +152,15 @@ public class ProductionApi extends BaseController<ProductionInfo,ProductionApiVO
 
 
 
-    @GetMapping("/delStatusByCode")
+    @PostMapping("/delStatusByCode")
     @ApiOperation(value="删除作品（变更作品状态为已删除）")
     @ApiImplicitParam(paramType="get", name = "code", value = "作品编码", required = true, dataType = "String")
-    public ApiResponse<Boolean> delStatusByCode(@RequestParam("code") String productionCode){
+    public ApiResponse<Boolean> delStatusByCode(@RequestBody @Validated(value = {ProductionApiVO.DelStatusByCode.class}) ProductionApiVO apiVO){
         ProductionInfo updateParam = new ProductionInfo();
-        updateParam.setCode(productionCode);
+        updateParam.setCode(apiVO.getCode());
         updateParam.setStatus(ProductionStatus.DELETED.getCode());
         if(productionInfoService.updateStatus(updateParam)){
-            return failed(String.format("删除%s作品失败",productionCode));
+            return failed(String.format("删除%s作品失败",apiVO.getCode()));
         }
         return success(Boolean.TRUE);
     }
@@ -152,6 +169,67 @@ public class ProductionApi extends BaseController<ProductionInfo,ProductionApiVO
     @Override
     protected Service<ProductionInfo> service() {
         return productionInfoService;
+    }
+
+
+    @Override
+    protected ProductionApiVO convert(ProductionInfo model) {
+        ProductionApiVO form = super.convert(model);
+        //转换枚举值
+        form.setStatusName(ProductionStatus.get(model.getStatus()).getName());
+        //岗位名称
+        BdJobCate bdJobCate = model.getBdJobCate();
+        if (bdJobCate != null) {
+            form.setJobCateName(bdJobCate.getCateName());
+        }
+        //技能标签 树路径
+        if(!CollectionUtils.isEmpty(model.getProductionSkillRelations())){
+            form.setJobs(model.getProductionSkillRelations().stream().map(productionSkillRelation ->
+                    JsonUtil.string2Obj(productionSkillRelation.getSkillTreePath(),new TypeReference<List<Long>>(){}))
+                    .collect(Collectors.toList()));
+        }
+        //附件列表
+        if(!CollectionUtils.isEmpty(model.getAttachmentInfos())){
+            form.setAttachmentInfos(model.getAttachmentInfos().stream().map(attachmentInfo ->{
+                        AttachmentInfoApiVO attachmentInfoApiVO = new AttachmentInfoApiVO();
+                        BeanUtils.copyProperties(attachmentInfo,attachmentInfoApiVO);
+                        return attachmentInfoApiVO;
+                    }).collect(Collectors.toList()));
+        }
+        //作者 自由职业者
+        if(model.getFreelancerInfo()!=null){
+            FreelancerInfoApiVO freelancerInfoApiVO = new FreelancerInfoApiVO();
+            BeanUtils.copyProperties(model.getFreelancerInfo(),freelancerInfoApiVO);
+            form.setFreelancerInfo(freelancerInfoApiVO);
+        }
+
+        return form;
+    }
+
+    @Override
+    protected ProductionInfo convert(ProductionApiVO form) {
+        ProductionInfo productionInfo = super.convert(form);
+        //现场景只允许选择同一个岗位下的技能
+        List<List<Long>> jobs = form.getJobs();
+        //获取岗位id
+        Long jobCateId = 0L;
+        //获取技能
+        List<ProductionSkillRelation> productionSkillRelations = new ArrayList<>();
+        productionInfo.setProductionSkillRelations(productionSkillRelations);
+        for( List<Long> job : jobs ){
+
+            ProductionSkillRelation productionSkillRelation = new ProductionSkillRelation();
+            productionSkillRelation.setJobSkillId(job.get(job.size() - 1));
+            productionSkillRelation.setSkillTreePath(JsonUtil.obj2String(job));
+            productionSkillRelations.add(productionSkillRelation);
+            //现阶段只支持 一个岗位下的技能数据
+            jobCateId = job.get(job.size() - 2);
+        };
+        productionInfo.setJobCateId(jobCateId);
+
+
+
+        return productionInfo;
     }
 }
 
