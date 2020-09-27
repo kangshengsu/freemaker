4fd42a018cb146b3b378427f3b2836f3e7a44056
hubo <hubo02090209@sina.com>
2020-09-28 00:10:26 +0800

    打印日志

--- api-gw/src/main/resources/shell/stop.sh
+++ api-gw/src/main/resources/shell/stop.sh
@@ -4,6 +4,10 @@ JAR_NAME=`ls | grep api-gw-*.jar`
 
 echo "jar名称：${JAR_NAME}"
 
+PID=`ps -ef | grep '${JAR_NAME}' | grep -v grep | awk '{print $2}' `
+
+echo "java 进程ID：${PID}"
+
 ps -ef | grep '${JAR_NAME}' | grep -v grep | awk '{print $2}' | xargs kill
 
 sleep 10s
