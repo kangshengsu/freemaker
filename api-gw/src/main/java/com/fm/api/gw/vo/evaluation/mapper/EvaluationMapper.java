e1bc4a11c5c21ff3e58e7a2b2b0085b93af89a97
zhangleqi <463456910@qq.com>
2020-10-09 20:44:26 +0800

    mapper改为抽象类

--- api-gw/src/main/java/com/fm/api/gw/vo/evaluation/mapper/EvaluationMapper.java
+++ api-gw/src/main/java/com/fm/api/gw/vo/evaluation/mapper/EvaluationMapper.java
@@ -1,16 +1,15 @@
 package com.fm.api.gw.vo.evaluation.mapper;
 
 import com.fm.api.gw.vo.evaluation.EvaluationInfoVO;
-import com.fm.api.gw.vo.production.list.ProductionListVO;
 import com.fm.api.gw.vo.production.relation.AttachmentVO;
 import com.fm.business.base.model.AttachmentInfo;
 import com.fm.business.base.model.evaluation.EvaluationInfo;
 import com.fm.framework.core.service.FileService;
-import com.fm.framework.core.utils.SpringUtil;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 
 /**
@@ -18,25 +17,28 @@ import org.springframework.util.StringUtils;
  * @date 2020/10/8
  */
 @Mapper(componentModel = "spring")
-public interface EvaluationMapper {
+public abstract class EvaluationMapper {
 
+
+    @Autowired
+    private FileService fileService;
 
     @Mapping(target = "images", source = "attachmentInfos")
-    EvaluationInfoVO toEvaluationListVO(EvaluationInfo evaluationInfo);
+    public abstract EvaluationInfoVO toEvaluationListVO(EvaluationInfo evaluationInfo);
 
 
     @Mappings({
             @Mapping(source = "path", target = "fullPath", qualifiedByName="fullPath"),
             @Mapping(source = "otherPath", target = "fullOtherPath", qualifiedByName="fullPath")
     })
-    AttachmentVO toAttachmentVO(AttachmentInfo attachmentInfo);
+    abstract AttachmentVO toAttachmentVO(AttachmentInfo attachmentInfo);
 
 
 
     @Named("fullPath")
-    default String fullPath(String path) {
+    String fullPath(String path) {
         if (!StringUtils.isEmpty(path)) {
-            return SpringUtil.getBean(FileService.class).getFullPath(path);
+            return fileService.getFullPath(path);
         }
         return null;
     }
