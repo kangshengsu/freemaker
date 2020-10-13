1dfee5422e34a17e90841340e69bc221a2764c6f
Zyc <Zyc@qq.com>
2020-10-13 23:45:04 +0800

    作品发布时间格式修改

--- api-gw/src/main/java/com/fm/api/gw/vo/production/view/ProductionViewVO.java
+++ api-gw/src/main/java/com/fm/api/gw/vo/production/view/ProductionViewVO.java
@@ -49,7 +49,7 @@ public class ProductionViewVO extends ProductionListVO implements Serializable {
     /**
      * 创建时间
      **/
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern="yyyy-MM-dd",timezone = "GMT+8")
     private Date createTime;
 
     private Long jobCateId;
