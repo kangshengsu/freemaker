62baaf643d245c992680b64caa0d748ae1a35af8
qdl <18406459159@163.com>
2020-10-29 19:47:58 +0800

    添加推荐码

--- api-web/src/main/resources/db_scrpit/change_ddl.sql
+++ api-web/src/main/resources/db_scrpit/change_ddl.sql
@@ -6,3 +6,10 @@ alter table order_info add COLUMN is_upload_voucher tinyint(4) NOT NULL DEFAULT 
 -- ----------------------------
 ALTER TABLE `freelancer_info`
 ADD COLUMN `referral_code`  varchar(64) NULL DEFAULT '' COMMENT '推荐码' AFTER `phone`
+
+-- ----------------------------
+-- Table alter for order_info
+-- 201029 订单表新增预算计算方式字段
+-- ----------------------------
+ALTER TABLE `order_info`
+ADD COLUMN `budget_type`  TINYINT(4) NULL DEFAULT 0 COMMENT '预算计算方式:0:时薪,1:一口价,2:面谈' AFTER `status`
