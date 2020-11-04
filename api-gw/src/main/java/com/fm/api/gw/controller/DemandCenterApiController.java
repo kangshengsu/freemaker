1f435ed0783503025bebacce37c1ac9cfea82a01
mac <405227329@qq.com>
2020-11-04 16:36:36 +0800

    需求中心查询

---
+++ api-gw/src/main/java/com/fm/api/gw/controller/DemandCenterApiController.java
@@ -0,0 +1,120 @@
+package com.fm.api.gw.controller;
+
+import com.fm.api.gw.vo.DemandInfoVO;
+import com.fm.business.base.enums.BudgetType;
+import com.fm.business.base.enums.DeliveryType;
+import com.fm.business.base.enums.DemandStatus;
+import com.fm.business.base.model.EmployerInfo;
+import com.fm.business.base.model.demand.DemandInfo;
+import com.fm.business.base.model.job.BdJobCate;
+import com.fm.business.base.service.IBdJobCateService;
+import com.fm.business.base.service.IEmployerInfoService;
+import com.fm.business.base.service.demand.IDemandCenterInfoService;
+import com.fm.business.base.service.demand.IDemandInfoService;
+import com.fm.business.base.service.order.IOrderInfoService;
+import com.fm.framework.core.Context;
+import com.fm.framework.core.query.Page;
+import com.fm.framework.core.query.PageInfo;
+import com.fm.framework.core.service.Service;
+import com.fm.framework.web.controller.BaseController;
+import com.fm.framework.web.response.ApiResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/v1/demandCenterApi")
+@Api(value = "需求中心接口")
+public class DemandCenterApiController extends BaseController<DemandInfo, DemandInfoVO> {
+
+    @Autowired
+    private IDemandCenterInfoService demandCenterInfoService;
+
+    @Autowired
+    private IEmployerInfoService iEmployerInfoService;
+
+    @Autowired
+    private IBdJobCateService iBdJobCateService;
+
+    @Autowired
+    private IOrderInfoService iOrderInfoService;
+
+    /**
+     *
+     * @param currentPage
+     * @param pageSize
+     * @param status
+     * @param jobCateId
+     * @return
+     */
+    @ApiOperation(value = "需求中心信息分页查询")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "currentPage", value = "当前页", dataType = "String", paramType = "query"),
+            @ApiImplicitParam(name = "pageSize", value = "页大小", dataType = "Integer", paramType = "query"),
+            @ApiImplicitParam(name = "jobCateId", value = "领域id", dataType = "Integer", paramType = "query")})
+    @RequestMapping(value = "getDemandCenterPage", method = RequestMethod.GET)
+    public ApiResponse<Page<DemandInfoVO>> getDemandCenterPage(@RequestParam("currentPage") Integer currentPage,
+                                                               @RequestParam("pageSize") Integer pageSize,
+                                                               @RequestParam(value = "status", required = false) Integer status,
+                                                               @RequestParam(value = "jobCateId", required = false) Integer jobCateId) {
+        Long currEmployerId = Context.getCurrEmployerId();
+        Page<DemandInfoVO> result = new PageInfo<>();
+        Page<DemandInfo> demandInfoPage = demandCenterInfoService.getDemandCenterPage(currentPage, pageSize, status , jobCateId);
+        if (demandInfoPage.getData().size() == 0) {
+            return ApiResponse.ofSuccess(result);
+        }
+        return success(this.convert(demandInfoPage));
+    }
+
+    /**
+     *
+     * @param code
+     * @return
+     */
+    @ApiOperation(value = "按需求编码获取需求中心详细信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "code", value = "需求编码", dataType = "String", paramType = "query")})
+    @RequestMapping(value = "getDemandCenterDtlByCode", method = RequestMethod.GET)
+    public ApiResponse<DemandInfoVO> getDemandCenterDtlByCode(@RequestParam(value = "code", required = false) String code) {
+        Long currEmployerId = Context.getCurrEmployerId();
+        DemandInfo demandInfoPage = demandCenterInfoService.getDemandCenterDtlByCode(code);
+        return success(this.convert(demandInfoPage));
+    }
+
+    @Override
+    protected Service<DemandInfo> service() {
+        return demandCenterInfoService;
+    }
+
+    @Override
+    protected DemandInfoVO convert(DemandInfo model) {
+        if (Objects.isNull(model)) {
+            return null;
+        }
+        DemandInfoVO form = super.convert(model);
+        //转换枚举值
+        form.setStatusName(DemandStatus.get(model.getStatus()).getName());
+        form.setDeliveryTypeName(DeliveryType.getNameByCode(model.getDeliveryType()));
+        form.setBudgetTypeName(BudgetType.getNameByCode(model.getBudgetType()));
+        //获取作者数据
+        EmployerInfo employerInfo = iEmployerInfoService.get(model.getEmployerId());
+        if (employerInfo != null) {
+            form.setEmployerName(employerInfo.getName());
+        }
+        //获取需求信息
+        BdJobCate bdJobCate = iBdJobCateService.get(model.getJobCateId());
+        if (bdJobCate != null) {
+            form.setJobCateIdName(bdJobCate.getCateName());
+        }
+        form.setOrderCount(iOrderInfoService.getOrderCountByDemandId(form.getId()));
+        return form;
+    }
+}
---
+++ business-base/src/main/java/com/fm/business/base/service/demand/IDemandCenterInfoService.java
@@ -0,0 +1,17 @@
+package com.fm.business.base.service.demand;
+
+import com.fm.business.base.model.demand.DemandInfo;
+import com.fm.framework.core.query.Page;
+import com.fm.framework.core.service.Service;
+
+public interface IDemandCenterInfoService extends Service<DemandInfo> {
+
+    /**
+     * @param currentPage 当前页
+     * @param pageSize    页大小
+     * @return 需求编码
+     */
+    Page<DemandInfo> getDemandCenterPage(Integer currentPage, Integer pageSize, Integer status , Integer jobCateId);
+
+    DemandInfo getDemandCenterDtlByCode(String code);
+}
