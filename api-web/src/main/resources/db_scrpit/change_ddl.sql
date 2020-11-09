992cad9ddfabf5bace70872c2e4e354e25947217
qdl <18406459159@163.com>
2020-11-09 18:28:40 +0800

    更新数据库脚本

--- api-web/src/main/resources/db_scrpit/change_ddl.sql
+++ api-web/src/main/resources/db_scrpit/change_ddl.sql
@@ -1,3 +1,14 @@
 ALTER TABLE employer_info
 ADD COLUMN `company` VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT '公司名称' AFTER `phone`,
 ADD COLUMN `job_title` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT '职务名称' AFTER `phone`;
+
+/**
+添加推荐人字段
+ */
+ALTER TABLE
+`freelancer_info`
+ADD COLUMN referrer COMMENT '推荐人' AFTER referral_code
+
+/**
+修改邀请码字段长度
+ */
