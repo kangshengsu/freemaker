065ac97932326e2de99a71ef02b518d2748f2d75
mac <405227329@qq.com>
2020-11-11 13:17:31 +0800

    修改公司职务2

--- api-gw/src/main/java/com/fm/api/gw/controller/demand/DemandApiController.java
+++ api-gw/src/main/java/com/fm/api/gw/controller/demand/DemandApiController.java
@@ -1,6 +1,7 @@
 package com.fm.api.gw.controller.demand;
 
 import com.fm.api.gw.vo.demand.DemandInfoVO;
+import com.fm.api.gw.vo.demand.mapper.DemandInfoMapper;
 import com.fm.business.base.enums.BudgetType;
 import com.fm.business.base.enums.DeliveryType;
 import com.fm.business.base.enums.DemandStatus;
@@ -69,6 +70,9 @@ public class DemandApiController extends BaseController<DemandInfo, DemandInfoVO
 
     @Autowired
     private IProductionInfoService iProductionInfoService;
+
+    @Autowired
+    private DemandInfoMapper demandInfoMapper;
 
     @ApiOperation(value = "获取需求分页信息")
     @ApiImplicitParams({
@@ -228,7 +232,7 @@ public class DemandApiController extends BaseController<DemandInfo, DemandInfoVO
         if (Objects.isNull(model)) {
             return null;
         }
-        DemandInfoVO form = super.convert(model);
+        DemandInfoVO form = demandInfoMapper.toDemandInfoVO(model);
         //转换枚举值
         form.setStatusName(DemandStatus.get(model.getStatus()).getName());
         form.setDeliveryTypeName(DeliveryType.getNameByCode(model.getDeliveryType()));
