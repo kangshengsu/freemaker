128992d09d083eac48fe4ab91fa517d4181924e6
qdl <18406459159@163.com>
2020-11-16 21:00:23 +0800

    修改数据库脚本

--- api-web/src/main/resources/db_scrpit/change_ddl.sql
+++ api-web/src/main/resources/db_scrpit/change_ddl.sql
@@ -1,19 +1,4 @@
 
 
--- 修改结果打分为响应速度
-ALTER TABLE `evaluation_info`
-CHANGE result_score response_speed DOUBLE COMMENT '响应速度（5分制）'
-
-
--- 修改过程打分为沟通能力
-ALTER TABLE `evaluation_info`
-CHANGE process_score  communicate_capacity DOUBLE COMMENT '沟通能力（5分制）'
-
--- 审核状态字段修改名称
-ALTER TABLE
-`evaluation_info`
-CHANGE audit_status `status` TINYINT(4) DEFAULT 10
-
-
 
 
