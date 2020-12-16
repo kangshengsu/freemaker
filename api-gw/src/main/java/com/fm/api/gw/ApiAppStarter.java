5ced16bb51f93cc9de100b25b99351a0d68419af
qdl <18406459159@163.com>
2020-12-16 10:14:50 +0800

    修改启动项配置

--- api-gw/src/main/java/com/fm/api/gw/ApiAppStarter.java
+++ api-gw/src/main/java/com/fm/api/gw/ApiAppStarter.java
@@ -3,6 +3,7 @@ package com.fm.api.gw;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.annotation.EnableCaching;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @MapperScan("com.fm.**.dao")
 @EnableCaching
 @EnableSwagger2
+@ConditionalOnProperty(name = "swagger.enable",havingValue = "true")
 public class ApiAppStarter {
 
     public static void main(String[] args) {
