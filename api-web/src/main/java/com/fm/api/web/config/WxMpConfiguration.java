00c3277b1e8da65ec92f0a6f91751c4d12ae5e44
mac <405227329@qq.com>
2020-12-21 16:14:49 +0800

    修改公众号注册10

--- api-web/src/main/java/com/fm/api/web/config/WxMpConfiguration.java
+++ api-web/src/main/java/com/fm/api/web/config/WxMpConfiguration.java
@@ -3,6 +3,7 @@ package com.fm.api.web.config;
 import lombok.AllArgsConstructor;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @AllArgsConstructor
 @Configuration
+@EnableConfigurationProperties(WxMpProperties.class)
 public class WxMpConfiguration {
 
     @Bean
